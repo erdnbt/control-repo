c6487c8b455802efe86b6ee1805ca6590e79338d
Erdenebat Turmunkh <t.erdenebat@gmail.com>
2021-10-10 23:11:24 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,22 @@
 node default {
-  include nginx
+    file { '/root/README' :
+        ensure => file,
+        content => 'This is a README',
+        owner => 'root'
+    }
+}
+node 'master.puppet.vm' {
+    include role::master_server
+    file { '/root/README':
+        ensure => file,
+        content => "Welcome to ${fqdn}\n",
+    }
+}
+
+node /^web/ {
+    include role::app_server
+}
+
+node /^db/ {
+    include role::db_server
 }
