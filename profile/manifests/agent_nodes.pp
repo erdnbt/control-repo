149046569142e2d7625acde5f6f37cb20038e35d
Erdenebat Turmunkh <t.erdenebat@gmail.com>
2021-10-10 23:05:54 +0200

    Delete profile directory

--- profile/manifests/agent_nodes.pp
+++
@@ -1,8 +0,0 @@
-class profile::agent_nodes {
-    include dockeragent
-    dockeragent::node { 'web.puppet.vm': }
-    host {'web.puppet.vm':
-        ensure  => present,
-        ip      => '10.10.10.10',
-    }
-}
