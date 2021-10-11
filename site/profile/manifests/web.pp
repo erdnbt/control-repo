7c3e59c3cffda4999c0c84b9c5d16782fa26eae8
erdnbt <t.erdenebat@gmail.com>
2021-10-11 20:06:52 +0200

    add details

--- site/profile/manifests/web.pp
+++ site/profile/manifests/web.pp
@@ -1,3 +1,16 @@
 class profile::web {
   include nginx
+  
+  class { 'nginx':
+    manage_repo     => true,
+    package_source  => 'nginx-stable'
+  } 
+  
+  nginx::resource::server { 'https://domain.com':
+    www_root  => '/opt/html/',
+  }
+
+  nginx::resource::location{'/resource':
+    proxy     => 'http://20.20.20.20/' ,
+    server    => 'https://domain.com'
 }
